cf594a8671f585328b6c9e1e4883fff906ccc37c
Ali-Kalbaev <aa2321714@gmail.com>
2022-10-28 15:49:38 +0300

    lll

---
+++ app.py
@@ -0,0 +1,32 @@
+import requests
+from flask import Flask, render_template, request
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(database="service_db",
+                        user="postgres",
+                        password="lyk147258369",
+                        host="localhost",
+                        port="5432")
+
+cursor = conn.cursor()
+
+
+@app.route('/login/', methods=['GET'])
+def index():
+    return render_template('login.html')
+
+
+@app.route('/login/', methods=['POST'])
+def login():
+    username = request.form.get('username')
+    password = request.form.get('password')
+    if username == '' or password == '':
+        error = 'Ошибка'
+        return render_template('login.html', error=error)
+
+    cursor.execute("SELECT * FROM service.users WHERE login=%s AND password=%s", (str(username), str(password)))
+    records = list(cursor.fetchall())
+    print(records)
+    return render_template('account.html', full_name=records[0][1], username=records[0][2], password=records[0][3])
